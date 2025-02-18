1bc44cce3f2ad261ebe938c94261d83e18544191
Priya.Sharma <priyasharma2702@gmail.com>
2025-02-18 23:52:01 +0530

    one more throttle

--- throttling.js
+++ throttling.js
@@ -11,9 +11,31 @@ function throttle(func, limit) {
   };
 }
 
-// Usage example:
+// Usage example:x`
 function handleScroll() {
   console.log("Scroll event handled");
 }
 
 window.addEventListener("scroll", throttle(handleScroll, 200));
+
+function myThrottle(func, limit) {
+  let lastFunc;
+  let lastRan;
+
+  return function (...args) {
+    const context = this;
+
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
