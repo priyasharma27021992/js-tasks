9aae6418d24ef36026a57b76860f563ef5f06eb1
Priya.Sharma <priyasharma2702@gmail.com>
2025-02-25 21:57:35 +0530

    flat obj second time written

--- flattenObj.js
+++ flattenObj.js
@@ -1,13 +1,27 @@
+// const myDeepFlattenObject = (obj, prefix = "") => {
+//   return Object.keys(obj).reduce((acc, key) => {
+//     const newKey = prefix ? `${prefix}.${key}` : key;
+//     if (typeof obj[key] === "object" && obj[key]) {
+//       Object.assign(acc, myDeepFlattenObject(obj[key], newKey));
+//     } else {
+//       acc[newKey] = obj[key];
+//     }
+//     return acc;
+//   }, {});
+// };
+
+// console.log(myDeepFlattenObject({ a: { b: { c: 2 } } }));
+
 const myDeepFlattenObject = (obj, prefix = "") => {
-  return Object.keys(obj).reduce((acc, key) => {
-    const newKey = prefix ? `${prefix}.${key}` : key;
-    if (typeof obj[key] === "object" && obj[key]) {
+  return Object(keys).reduce((acc, key) => {
+    let newKey = prefix ? `${prefix}.${key}` : key;
+    if (typeof obj[key] === "object" && obj[key] !== null) {
       Object.assign(acc, myDeepFlattenObject(obj[key], newKey));
     } else {
       acc[newKey] = obj[key];
     }
     return acc;
-  }, {});
+  });
 };
 
 console.log(myDeepFlattenObject({ a: { b: { c: 2 } } }));
